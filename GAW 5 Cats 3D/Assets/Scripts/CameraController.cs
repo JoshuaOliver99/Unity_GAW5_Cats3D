015d67b32df1ad19561147437cd283a9ab22a8f7
JoshuaOliver99 <56636418+JoshuaOliver99@users.noreply.github.com>
2020-02-26 14:48:33 +0000

    Map Prefab Error - RaycastPlane removal
    Broken map prefab, unable to loadmap

---
+++ GAW 5 Cats 3D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform camera;
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            camera.position += new Vector3(0, -1, 0);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            camera.position += new Vector3(0, 1, 0);
+        }
+    }
+}

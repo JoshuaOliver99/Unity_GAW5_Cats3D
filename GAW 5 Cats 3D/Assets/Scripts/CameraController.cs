11fa1b34e7a467aa6613ecfa27d2585ff4ebac02
JoshuaOliver99 <56636418+JoshuaOliver99@users.noreply.github.com>
2020-02-29 13:17:27 +0000

    Submitted state
    Visible map
    Territory quadrants
    Movable player

--- GAW 5 Cats 3D/Assets/Scripts/CameraController.cs
+++ GAW 5 Cats 3D/Assets/Scripts/CameraController.cs
@@ -17,5 +17,27 @@ public class CameraController : MonoBehaviour
         {
             camera.position += new Vector3(0, 1, 0);
         }
+
+        if (Input.GetKey(KeyCode.A)) // left
+        {
+            camera.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D)) // right
+        {
+            camera.position += new Vector3(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W)) // up
+        {
+            camera.position += new Vector3(0, 0, 1);
+        }
+        else if (Input.GetKey(KeyCode.S)) // down
+        {
+            camera.position += new Vector3(0, 0, -1);
+        }
+
+        if (Input.GetKey(KeyCode.Escape)) // quit
+        {
+            Application.Quit();
+        }
     }
 }
---
+++ GAW 5 Cats 3D/Assets/Scripts/Territory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Territory : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerCharacter;
+
+    public float territoryControl = 50;
+    bool isOwned;
+
+    private void Start()
+    {
+        if (territoryControl < 100)
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        else if (territoryControl < 200)
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    void Update()
+    {
+        if (playerCharacter.transform.position.x > (transform.position.x - 25) // If Player inside the zone bounds
+            && playerCharacter.transform.position.x < (transform.position.x + 25)
+            && playerCharacter.transform.position.z > (transform.position.z - 25)
+            && playerCharacter.transform.position.z < (transform.position.z + 25))
+        {
+            if (territoryControl <= 200) // If territoryControl Less than max
+                territoryControl += Time.deltaTime * 10;
+
+            if (territoryControl > 100) 
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.green;
+                isOwned = true;
+            }
+            else
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+                isOwned = false;
+            }
+
+        }
+
+
+    }
+}
